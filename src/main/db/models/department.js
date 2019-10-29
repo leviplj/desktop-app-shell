b427667b0a3d92a993b99848101043d28a48adee
leviplj <leviplj@gmail.com>
2019-10-29 21:27:23 +0000

    Add relation between department and product.

--- src/main/db/models/department.js
+++ src/main/db/models/department.js
@@ -2,6 +2,14 @@ module.exports = (sequelize, DataTypes) => {
   const Department = sequelize.define('department', {
     name: DataTypes.STRING(50)
   }, {})
+
+  Department.associate = (models) => {    
+    Department.hasMany(models.product, {
+      foreignKey: 'departmentId',
+      as: 'products',
+      onDelete: 'CASCADE',
+    })
+  }
   
   return Department
 }
--- src/main/db/models/product.js
+++ src/main/db/models/product.js
@@ -2,8 +2,15 @@ module.exports = (sequelize, DataTypes) => {
   const Product = sequelize.define('product', {
     name: DataTypes.STRING(50),
     price: DataTypes.DECIMAL(15, 4),
+    departmentId: DataTypes.INTEGER,
   }, {})
+
+  Product.associate = (models) => {    
+    Product.belongsTo(models.department, {
+      foreignKey: 'departmentId',
+      as: 'department',
+    })
+  }
   
   return Product
 }
-
